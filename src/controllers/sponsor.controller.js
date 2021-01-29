c5517aa5eb6b9a742b12eb6e14ac90031a6b20de
Ivan Gonzalez <gonzalez.ivand@gmail.com>
2021-01-29 16:48:19 -0500

    Add list and update methods to sponsor controller, and created an authentication middleware

--- src/controllers/sponsor.controller.js
+++ src/controllers/sponsor.controller.js
@@ -1,44 +1,102 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cryptRandomString = require('crypto-random-string');
 const Sponsor = require('../models/sponsor.model');
 
 
 module.exports = {
-  async show(req, res){
-    try{
-      const { sponsorId } = req.params;
-      const sponsor = await Sponsor.findById(sponsorId)
-      if(!sponsor){
+  async list(req, res) {
+    try {
+      const sponsors = await Sponsor.find()
+      if( sponsors.length === 0 ) {
+        throw new Error('Could not find any sponsors')
+      }
+      res.status(200).json( { message: 'sponsors found', data: sponsors } )
+    }
+    catch(err) {
+      res.status(400).json( { message: err.message } )
+    }
+  },
+  async show(req, res) {
+    try {
+      const id = req.userId;
+      const sponsor = await Sponsor.findById(id)
+      if(!sponsor) {
         throw new Error('Sponsor not found in the database')
       }
-      res.status(200).json({ message: "Sponsor found", data: sponsor })
+      res.status(200).json( { message: "Sponsor found", data: sponsor } )
     }
-    catch(err){
+    catch(err) {
+      res.status(400).json( { message: err.message } )
+    }
+  },
+  async signup( req, res) {
+    try {
+      const { email, password } = req.body;
+      if(password.length < 4 || password.length > 8) {
+        throw new Error('Your password must be between 4 and 8 characters long');
+      }
+      const encPassword = await bcrypt.hash( password, 8 );
+      const sponsor = await Sponsor.create( { email, password: encPassword } );
+
+      const token = jwt.sign(
+        { id: sponsor._id },
+        process.env.SECRET,
+        { expiresIn: 60 * 60 * 24 },
+      );
+
+      res.status(201).json( { token } )
+    }
+    catch(err) {
       res.status(400).json({ message: err.message })
     }
   },
-  async signup( req, res){
-    try{
+  async signin(req, res) {
+    try {
       const { email, password } = req.body;
-      if(password.length < 4 || password.length > 8){
-        throw new Error('Your password must be between 4 and 8 characters long')
+      const sponsor = await Sponsor.findOne( { email } );
+      if(!sponsor) {
+        throw new Error('Invalid email or password');
       }
-      const sponsor = await Sponsor.create({ email, password })
-      res.status(201).json(sponsor)
+      const isValid = await bcrypt.compare( password, sponsor.password );
+      if(!isValid) {
+        throw new Error('Invalid email or password');
+      }
+      const token = jwt.sign(
+        { id: sponsor._id },
+        process.env.SECRET,
+        { expiresIn: 60 * 60 * 24 },
+      );
+      res.status(201).json( { token } );
     }
-    catch(err){
-      res.status(400).json({ message: err.message })
+    catch(err) {
+      res.status(400).json( { message: err.message } );
     }
   },
   async destroy(req,res) {
-    const  { sponsorId } = req.params;
     try {
-      const sponsor = await Sponsor.findByIdAndDelete(sponsorId)
-      if(!sponsor){
+      const id = req.userId;
+      const sponsor = await Sponsor.findByIdAndDelete(id)
+      if(!sponsor) {
         throw new Error('Sponsor not found in the database')
       }
-      res.status(200).json({ message: 'Sponsor Deleted', data: sponsor })
+      res.status(200).json( { message: 'Sponsor Deleted', data: sponsor } )
     }
-    catch(err){
-      res.status(400).json({ message: err.message })
+    catch(err) {
+      res.status(400).json( { message: err.message } )
+    }
+  },
+  async update(req, res) {
+    try {
+      const id = req.userId;
+      const sponsor = await Sponsor.findByIdAndUpdate( id, req.body, { new: true, runValidators: true } )
+      if(!sponsor){
+        throw new Error('Sponsor not found')
+      }
+      res.status(200).json( { message: 'Sponsor Information Updated', data: sponsor } )
+    }
+    catch(err) {
+      res.status(400).json( { message: err.message } )
     }
   }
 }
--- src/routes/sponsor.js
+++ src/routes/sponsor.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
+const { auth } = require('../utils/auth');
 
 const sponsorController = require('../controllers/sponsor.controller')
 
-router.route('/').post(sponsorController.signup)
-router.route('/:sponsorId').get(sponsorController.show)
-router.route('/:sponsorId').delete(sponsorController.destroy)
+router.route('/signup').post(sponsorController.signup)
+router.route('/signin').post(sponsorController.signin)
+router.route('/').get(sponsorController.list)
+router.route('/:sponsorId').get(auth, sponsorController.show)
+router.route('/:sponsorId').put(auth, sponsorController.update)
+router.route('/:sponsorId').delete(auth, sponsorController.destroy)
 
 module.exports = router;
 
