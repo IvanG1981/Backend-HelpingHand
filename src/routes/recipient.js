ea2168c96bad38db69c50a0d4e1833a639d1dad3
Ivan Gonzalez <gonzalez.ivand@gmail.com>
2021-02-01 13:07:38 -0500

    Add contributions field to sponsor and recipient models

--- src/routes/recipient.js
+++ src/routes/recipient.js
@@ -7,7 +7,7 @@ router.route('/').post(auth, recipientController.create);
 router.route('/').get(recipientController.list);
 router.route('/:recipientId').get(recipientController.show);
 router.route('/update/:recipientId').put(auth, recipientController.update);
-router.route('/updateNeed/:recipientId').put(recipientController.updateNeed);
+// router.route('/updateNeed/:recipientId').put(recipientController.updateNeed);
 router.route('/remove/:recipientId').delete(auth, recipientController.destroy);
 
 
