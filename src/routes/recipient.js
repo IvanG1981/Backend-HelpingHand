31e94d20bef87adcdc663c84349aadbdbc5a2906
Ivan Gonzalez <gonzalez.ivand@gmail.com>
2021-02-01 13:08:18 -0500

    Remove updateNeed route from recipient routes, because this action will be performed from contributions controller

--- src/routes/recipient.js
+++ src/routes/recipient.js
@@ -7,7 +7,6 @@ router.route('/').post(auth, recipientController.create);
 router.route('/').get(recipientController.list);
 router.route('/:recipientId').get(recipientController.show);
 router.route('/update/:recipientId').put(auth, recipientController.update);
-// router.route('/updateNeed/:recipientId').put(recipientController.updateNeed);
 router.route('/remove/:recipientId').delete(auth, recipientController.destroy);
 
 
