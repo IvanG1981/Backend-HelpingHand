ed51a73d727034f021724cfc0f5a67431056708e
Ivan Gonzalez <gonzalez.ivand@gmail.com>
2021-02-05 17:02:14 -0500

    Add recipientId to mobileCOntribution route

--- src/routes/contribution.js
+++ src/routes/contribution.js
@@ -4,7 +4,7 @@ const { mobileEpayco } = require('../utils/mobileEpayco');
 
 const contributionController = require('../controllers/contribution.controller');
 
-router.route('/mobile').post(mobileEpayco, contributionController.createMobileContribution);
+router.route('/mobile/:recipientId').post(auth, mobileEpayco, contributionController.createMobileContribution);
 router.route('/').get(auth, contributionController.list);
 router.route('/:recipientId').post(auth, contributionController.create);
 
