2a29002f217955007a0542b15f3ea62b811f1a6b
Ivan Gonzalez <gonzalez.ivand@gmail.com>
2021-02-08 19:43:01 -0500

    Create formData middleware to handle image upload

--- src/routes/recipient.js
+++ src/routes/recipient.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const { auth } = require('../utils/auth');
+const  formData  = require('../utils/formData');
 
 const recipientController = require('../controllers/recipient.controller')
 
-router.route('/').post(auth, recipientController.create);
+router.route('/').post(auth, formData, recipientController.create);
 router.route('/').get(recipientController.list);
 router.route('/:recipientId').get(recipientController.show);
 router.route('/update/:recipientId').put(auth, recipientController.update);
